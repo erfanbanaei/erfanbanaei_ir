3d9d02f278562249ae50c884b2746633cdfb3707
erfanbanaei <mr.erfanbanaei@gmail.com>
2025-07-23 10:23:18 +0330

    🔄 Update import path in notifySnackBar.dart to use a relative path, ensuring consistency with the new project structure.

--- lib/functions/notifySnackBar.dart
+++ lib/functions/notifySnackBar.dart
@@ -1,4 +1,4 @@
-import 'package:nakul_dev/styles/styles.dart';
+import '../../styles/styles.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
